d5695160bd44c3b93b444462b52847089d872fc7
Gustav-Henrique <116670913+Gustav-Henrique@users.noreply.github.com>
2023-09-15 12:05:17 -0300

    Add files via upload

---
+++ animais.js
@@ -0,0 +1,25 @@
+export default class Animal{
+    constructor(nome, idade, tipo, familia) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+        this.familia = familia;
+        this.alimentado = false;
+    }
+
+    alimentarAnimal() {
+        this.alimentado = true;
+    }
+
+    envelhecerAnimal(anos) {
+        this.idade = this.idade + anos;
+    }
+    jogarBola() {
+        console.log(`Você jogou bola com o animal ${this.nome} da raça ${this.tipo}`);
+    }
+
+    jogarGraveto() {
+        console.log(`Você jogou um Graveto para o animal ${this.nome} da raça ${this.tipo}`);
+    }
+}
+
---
+++ zoo.js
@@ -0,0 +1,12 @@
+export default class Zoo {
+    animais = [];
+
+    adicionarAnimal(animal) {
+        this.animais.push(animal);
+    }
+
+}
+
+const zoologico = new Zoo();
+
+
